0e212da4bb851e935fe3e59fc72750b6609cbabf
AndreyGrishchenko <65390885+AndreyGrishchenko@users.noreply.github.com>
2021-03-21 19:39:43 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/SepFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class SepFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb((int)(double)(sourceColor.R * 0.299)+2*100,
+                                                (int)((int)(double)(sourceColor.G * 0.299)+0.5*100),
+                                                (int)(double)(sourceColor.B * 0.299)-1*100);
+            return resultColor;
+        }
+    }
+}
